1d512ff009d69cd1e3f4cbb490e05509a321e813
Jose Miguel Paz Portilla <jpazportilla@frba.utn.edu.ar>
2023-04-01 11:05:35 -0300

    Modifique el archivo de configuraciones

--- 00-PrimerProgramaC/configuracion.h
+++ 00-PrimerProgramaC/configuracion.h
@@ -1,5 +1,10 @@
+#ifndef _CONFIGURACION_H_
+#define _CONFIGURACION_H_
+
 /*Solo debe esta descomentado el idioma que se va a utilizar*/
 
 /*#define IDIOMA_CASTELLANO*/
 /*#define IDIOMA_INGLES*/
 #define IDIOMA_FRANCES
+
+#endif
